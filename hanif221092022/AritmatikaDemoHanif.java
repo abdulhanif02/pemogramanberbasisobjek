262bb378eef2ffe21909b300031b9455aeab4bd3
Abdul Hanif <114059939+abdulhanif02@users.noreply.github.com>
2023-01-06 09:20:08 +0700

    Praktek Pemograman Berorientasi Objek

---
+++ hanif221092022/AritmatikaDemoHanif.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nip.p221092022;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class AritmatikaDemoHanif {
+    public static void main(String[] args) {
+        int i = 37;
+        int j = 42;
+        double x= 27.475;
+        double y= 7.22;
+        
+        System.out.println("Variable values...");
+        System.out.println("i ="+i);
+        System.out.println("j ="+j);
+        System.out.println("x ="+x);
+        System.out.println("y ="+y);
+        
+        System.out.println("Adding....");
+        System.out.println("i + j ="+(i+j));
+        System.out.println("x + y ="+(x+y));
+        
+        System.out.println("Subtracing....");
+        System.out.println("i - j ="+(i-j));
+        System.out.println("x - y ="+(x-y));
+        
+        System.out.println("Multipying...");
+        System.out.println("i * j = "+(i*j));
+        System.out.println("x * y = "+(x*y));
+        
+        System.out.println("Dividing...");
+        System.out.println("i / j = "+(i/j));
+        System.out.println("x / y = "+(x/y));
+        
+        System.out.println("Computing the remainder...");
+        System.out.println("i % j = "+(i%j));
+        System.out.println("x % y = "+(x%y));
+        
+        System.out.println("Mixing tipes...");
+        System.out.println("j + y = "+(j+y));
+        System.out.println("i * x = "+(i*x));
+    }
+}
---
+++ hanif221092022/RelasiDemoHanif.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nip.p221092022;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class RelasiDemoHanif {
+    public static void main(String[] args) {
+        int i = 37;
+        int j = 42;
+        int k = 42;
+        System.out.println("Variavel Value...");
+        System.out.println("i = "+i);
+        System.out.println("j = "+j);
+        System.out.println("k = "+k);
+        
+        System.out.println("Grater than...");
+        System.out.println("i > j = "+(i > j));
+        System.out.println("j > i = "+(j > i));
+        System.out.println("k > j = "+(k > j));
+        
+        System.out.println("Greater than or equal to...");
+        System.out.println("i >= j = "+(i>=j));
+        System.out.println("j >= i = "+(j>=i));
+        System.out.println("k >= j = "+(k>=j));
+        
+        System.out.println("Less than...");
+        System.out.println("i < j = "+(i<j));
+        System.out.println("j < i = "+(j<i));
+        System.out.println("k < j = "+(k<j));
+        
+        System.out.println("Less than or equal to...");
+        System.out.println(" i <= j = " + (i <= j));
+        System.out.println(" j <= i = " + (j <= i));
+        System.out.println(" k <= j = " + (k <= j));    
+        
+        System.out.println("Equal to...");
+        System.out.println(" i == j = " + (i == j));
+        System.out.println(" k == j = " + (k == j));
+        
+        System.out.println("Not equal to..."); 
+        System.out.println(" i != j = " + (i != j)); //true
+        System.out.println(" k != j = " + (k != j)); //false 
+    }
+}
